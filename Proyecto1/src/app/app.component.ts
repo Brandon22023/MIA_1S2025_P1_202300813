47d6c42cbb8cf74efdb8c5c3295fd4eec0d2366a
Brandon Antonio Marroquin Pérez <3045062060114@ingenieria.usac.edu.gt>
2025-03-16 15:53:05 -0600

    ahora si interfaz terminada toca hacer backend

--- Proyecto1/src/app/app.component.ts
+++ Proyecto1/src/app/app.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Proyecto1';
+  entrada: string = '';
+  salida: string = '';
+
+
+     // Referencia al elemento de entrada de archivo
+  @ViewChild('fileInput') fileInput!: ElementRef;
+  // Método que se llama cuando se selecciona un archivo
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      const reader = new FileReader();
+      // Evento que se llama cuando se ha leído el archivo
+      reader.onload = (e) => {
+        const text = reader.result as string;
+        this.entrada = text; // Asigna el contenido del archivo a la variable 'entrada'
+      };
+      this.entrada = '';
+      reader.readAsText(file); // Lee el archivo como texto
+    }
+  }
+  // Método para limpiar las variables 'entrada' y 'salida'
+  limpiar(): void {
+    this.entrada = '';
+    this.salida = '';
+  }
 }
